965d1416ca5eda6cfa09b0740a50c5f9500616fe
sweetbread <glebzaharov1@gmail.com>
2023-07-22 05:03:11 +0300

    Set title to Server's name

--- app/src/main/java/ru/sweetbread/flake/ServerActivity.kt
+++ app/src/main/java/ru/sweetbread/flake/ServerActivity.kt
@@ -3,6 +3,14 @@ package ru.sweetbread.flake
 import android.os.Bundle
 import androidx.appcompat.app.AppCompatActivity
 import com.discord.panels.OverlappingPanelsLayout
+import io.ktor.client.HttpClient
+import io.ktor.client.request.bearerAuth
+import io.ktor.client.request.get
+import io.ktor.client.request.headers
+import io.ktor.client.statement.bodyAsText
+import io.ktor.http.HttpStatusCode
+import kotlinx.coroutines.runBlocking
+import org.json.JSONObject
 
 class ServerActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
@@ -11,5 +19,19 @@ class ServerActivity : AppCompatActivity() {
 
         findViewById<OverlappingPanelsLayout>(R.id.overlapping_panels)
             .setEndPanelLockState(OverlappingPanelsLayout.LockState.CLOSE)
+
+        runBlocking {
+            val client = HttpClient()
+            val serverId = intent.extras!!.getString("server_id")
+            val token = getSharedPreferences("Account", 0).getString("token", null)!!
+
+            val request =
+                client.get("$baseurl/dev/servers/$serverId")
+                { headers { bearerAuth(token) } }
+            if (request.status == HttpStatusCode.OK) {
+                val json = JSONObject(request.bodyAsText())
+                title = json.getString("name")
+            }
+        }
     }
 }
